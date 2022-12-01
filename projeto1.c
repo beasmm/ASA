863f10eba433f670bfc0bdd2dbb6f74ac7854d5f
mucl3 <63361221+mucl3@users.noreply.github.com>
2022-12-01 13:22:43 +0000

    Add files via upload

---
+++ projeto1.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int line, column;
+
+
+int main(){
+    //scanf("%d", &line);
+    //scanf("%d", &column);
+    int tiles[line], n;
+    for(int i = line; i > 0; i--){
+        scanf("%d", &n);
+        tiles[i] = n;
+    }
+
+    for(int i = 0; i< line; i++){
+        int n = tiles[i];
+        printf("%d", n);
+    }
+
+    return 0;
+}
