8f5fb19df77b3c5e94ff5e71d71bb38030972588
mucl3 <63361221+mucl3@users.noreply.github.com>
2022-12-11 17:53:51 +0000

    Add files via upload
    sinto me mal

---
+++ test.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int CalcSqr(int lin, int cal, int path[]){
+
+    int Sqr = 0;
+
+    for (int i =0; i < lin ; i++){
+
+        if ((path[i] <= lin -1) && (Sqr < path[i]))
+            Sqr = path[i];
+        
+        if ((lin -1 <= path[i]) && (Sqr < lin-1))
+            Sqr = lin -1;
+    }
+
+    return Sqr;
+}
+
+int Cal(int lin, int cal, int path[], int sqr, int count){
+    for (int i = 0; i <lin ; i++){
+        for (int j = 0; j <= path[i]; j++){
+
+            if((lin -1 >= sqr) && (path[i]- j >= sqr))
+                count++;
+        }
+
+        if (((path[i]) / sqr >= 2) && (lin -1 >= sqr))
+            count = count + (path[i] / sqr) + (path[i] * (path[i] % sqr));
+    }
+    return count;
+}
+
+int main(){
+
+    int n, m;
+
+    cin >> n;
+    cin >> m;
+    int path[20];
+
+    for (int i =0; i < n; i++){
+        cin >> path[i];
+    }
+    int c = Cal(n, m, path, CalcSqr(n,m,path), 1);
+
+    cout << c;
+    return 0;
+}
