95c6cd0de5946d4e1c1a88132a3ae0618e9f3c16
mucl3 <63361221+mucl3@users.noreply.github.com>
2022-12-12 15:40:59 +0000

    solved maxArea

--- proj1.cpp
+++ proj1.cpp
@@ -8,19 +8,26 @@ int maxSqr(int n_lines, vector<int> path){
     int maxArea = 1;
 
     for (int i = 0; i < n_lines; i++) {
-        if (n_lines - (i+1) >= path[i] - 1) 
+        if (n_lines - (i+1) >= path[i] - 1) {
             maxArea = path[i];
+        }
+        else{
+            for(int j = 2; j < path[i]; j++){
+                if (n_lines - (i+1) >= j - 1 && maxArea < j){ 
+                   maxArea = j;
+                }
+            }
+        }
     }
-    
     return maxArea;
 }
 
 
-int isSquare(int line, int column) {
-    if (line * line == column * column) return 1;
-
-    return 0;
-}
+//int isSquare(int line, int column) {
+//    if (line * line == column * column) return 1;
+//
+//    return 0;
+//}
 
 
 int calcSq(int a) {
